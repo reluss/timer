d73f40f6ec338d3ae9722342275b0332aa018df1
reluss <paul_purkopp@gmx.de>
2020-10-23 03:29:07 +0200

    +2hours fixed

--- script backup.js
+++ script backup.js
@@ -1,24 +1,19 @@
 let x;
-let workingTime = 8;
-let pauseTime = 0.5;
 
-// display current date in datePicker
 const date = new Date();
-// date.setDate(date.getDate() + 1);
-document.getElementById("dateInput").valueAsDate = date;
+date.setDate(date.getDate() + 1);
 
+document.getElementById("dateInput").valueAsDate = date;
 
 document
   .getElementById("dateTimeInput")
-  .addEventListener("clocklet.closing", function timer() {
+  .addEventListener("clocklet.closing", function () {
 
-    // calculate working hours (8.5 hrs)
     Date.prototype.addHours = function(h) {
       this.setTime(this.getTime() + (h*60*60*1000));
       return this;
     }
 
-    // create DateTime-string
     combineDateAndTime = function(date, time) {
       // get input from datePicker 
       var inputDate = document.getElementById("dateInput").value;
@@ -27,41 +22,51 @@ document
       // get input from Clock
       var timeString = document.getElementById("dateTimeInput").value;
 
-      // combine date & time
+      //timeString = time.getHours() + ':' + time.getMinutes() + ':00';
+      //timeString = time.getHours() + ':' + time.getMinutes() + ':00';
+  
+      // var year = inputDate.getFullYear();
+      // var month = inputDate.getMonth() + 1; // Jan is 0, dec is 11
+      // var day = inputDate.getDate();
+
+      //var dateString = '' + year + '-' + month + '-' + day;
+      // var combined = new Date(dateString + ' ' + timeString);
       var combined = inputDate + ' ' + timeString;
   
       return combined;
-    };  
+  };
+  
     console.log("combinedTimeDate" + new Date(combineDateAndTime()));
     
+
 
     var input = combineDateAndTime();
     console.log(input);
 
-    var feierabend = new Date(input).addHours(workingTime + pauseTime);
+    var feierabend = new Date(input).addHours(8.5);
     console.log("feierabend" + feierabend);
 
-    //var now = new Date().getTime();
-    var today = new Date();
-    var now = today.setHours( today.getHours() + 2 );
-
-
+    var now = new Date().getTime();
     console.log(new Date(now));
 
 
-    clearInterval(x);
+    if (x) {
+      clearInterval(x);
+      console.log("test 1");
+    }
+    
     //---------------------------------------------------------------
-    x = setInterval(function() {    
+    var x = setInterval(function() {
+      
+
       // Get today's date and time
-      //var now = new Date().getTime();
-      var today = new Date();
-      var now = today.setHours( today.getHours() + 2 );
+      var now = new Date().getTime();
     
       // Find the distance between now and the count down date
       var distance = feierabend - now;
     
       // Time calculations for days, hours, minutes and seconds
-      //var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
@@ -82,7 +87,6 @@ document
         document.getElementById("remainderDaily").innerHTML = "Feierabend";
       }
     }, 1000);
-  
 
     //---------------------------------------------------------------
     
@@ -106,8 +110,6 @@ document
     document.getElementById("feierabend").innerHTML = timeString;
   });
 
-
-  
 document
   .getElementById("dateTimeInput2")
   .addEventListener("change", function () {
@@ -148,30 +150,31 @@ document
     // document.getElementById("iP").innerHTML = new date(timeDiff);
   });
 
-// /*
-//   document.getElementById("timepick").addEventListener("change", function () {
-//   var inputTime = this.value;
-//   // var dateEntered = new Date(input);
-//   //console.log(inputTime); //e.g. 2015-11-13
-//   // console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
+/*
+  document.getElementById("timepick").addEventListener("change", function () {
+  var inputTime = this.value;
+  // var dateEntered = new Date(input);
+  //console.log(inputTime); //e.g. 2015-11-13
+  // console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
 
-//   //document.getElementById("iP").innerHTML = this.value;
-// });
+  //document.getElementById("iP").innerHTML = this.value;
+});
 
-// // function diff_minutes(dt2, dt1) {
-// //   var diff = (dt2.getTime() - dt1.getTime()) / 1000;
-// //   diff /= 60;
-// //   return Math.abs(Math.round(diff));
-// // }
+// function diff_minutes(dt2, dt1) {
+//   var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+//   diff /= 60;
+//   return Math.abs(Math.round(diff));
+// }
 
-// document.getElementById("timepick2").addEventListener("change", function () {
-//   var inputTime2 = new Time(this.value);
-//   var inputTime = document.getElementById("timepick").value;
-//   //diff(inputTime2, inputTime);
-//   //var diff = diff(inputTime2, inputTime);
-//   //console.log(diff);
-//   console.log(inputTime2);
-//   var timeDiff = Math.abs(new Date(inputTime2) - new Date(inputTime));
-//   console.log(timeDiff);
-//   document.getElementById("iP").innerHTML = timeDiff;
-// });
+document.getElementById("timepick2").addEventListener("change", function () {
+  var inputTime2 = new Time(this.value);
+  var inputTime = document.getElementById("timepick").value;
+  //diff(inputTime2, inputTime);
+  //var diff = diff(inputTime2, inputTime);
+  //console.log(diff);
+  console.log(inputTime2);
+  var timeDiff = Math.abs(new Date(inputTime2) - new Date(inputTime));
+  console.log(timeDiff);
+  document.getElementById("iP").innerHTML = timeDiff;
+});
+*/
